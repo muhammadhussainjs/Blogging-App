88ccb4e9291b679bfe112108bb17031a016bd555
muhammadhussainharoon <hussain356q@gmail.com>
2023-11-30 12:52:02 +0500

    process

--- home.js
+++ home.js
@@ -1,8 +1,15 @@
 import { onAuthStateChanged , signOut } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js"
-import {auth} from "./config.js"
+import {auth , db} from "./config.js"
+import { collection, addDoc , Timestamp, query, getDocs, orderBy } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 
 
 const logout = document.querySelector('#logout')
+const text = document.querySelector('#texts')
+const area = document.querySelector('#textarea')
+const maindiv = document.querySelector('#maindiv')
+const form = document.querySelector('#form')
+const uidd = document.querySelector('#uid')
+
 
 
 
@@ -21,12 +28,50 @@ onAuthStateChanged(auth,  (user) => {
 //logout function
     logout.addEventListener('click' , ()=>{
         signOut(auth).then(() => {
-            setTimeout(() => {
+        
                 window.location = "index.html"
                 
-            }, 2000);
             
           }).catch((error) => {
               alert("error")
           });
+        })        
+
+//adddocs
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            try {
+                const postObj = {
+                    text: text.value,
+                    area: area.value,
+                    uid: auth.currentUser.uid,
+                    postDate: Timestamp.fromDate(new Date())
+                }
+                const docRef = await addDoc(collection(db, "post"), postObj);
+                console.log("Document written with ID: ", docRef.id);
+               
+            } catch (e) {
+                console.error("Error adding document: ", e);
+            }
         })
+
+
+//getdocs
+        async function getDataFromFirestore() {
+            const q = await query(collection(db, "post"), orderBy('postDate', 'desc'));
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                console.log(doc.data());
+            });
+        
+        
+        }
+        getDataFromFirestore()
+
+
+
+
+
+
+
+
--- signup.js
+++ signup.js
@@ -25,25 +25,42 @@ signin.addEventListener('click' , ()=>{
 
 form.addEventListener('submit' , (event)=>{
     event.preventDefault()
-    if(password.value === repeatpassword.value){
+    if(password.value !== repeatpassword.value){
+      alert('password are not same')
+      return
+    }
     createUserWithEmailAndPassword(auth, email.value, password.value)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    console.log(user);
-    window.location = "index.html"
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log(user);
+   
+    const file = img.files[0]
+    const storageRef = ref(storage, firstname.value);
+    uploadBytes(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then((url) => {
+            
+                    addDoc(collection(db, "post"), {
+                      email: email.value,
+                      Name: firstname.value,
+                      uid: user.uid,
+                      profileUrl: url
+                    }).then((res) => {
+                        console.log(res);
+                        window.location = 'index.html'
+                    }).catch((err) => {
+                        console.log(err);
+                    })
+                })
         })
   .catch((error) => {
-    const errorCode = error.code;
     const errorMessage = error.message;
     console.log(errorMessage)
   });
-}else{
-    alert('enter correct password')
-}
+
 firstname.value = ""
 lastname.value = ""
 email.value = ""
   password.value = ""
   repeatpassword.value = ""
 
-})
+})})
